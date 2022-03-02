8ff94faa424336c23e0ca6136d41a87bc964c91e
Felipe <fquiceno1@misena.edu.co>
2022-03-02 07:51:07 -0500

    Primer commit

---
+++ src/main/java/StartApp.java
@@ -0,0 +1,17 @@
+public class StartApp {
+    public static void main(String[] args) {
+
+        
+        Account account = new Account(new Statement());
+
+        account.deposit(amount(1000), date("10/01/2021"));
+        account.deposit(amount(2000), date("13/01/2021"));
+        account.withdrawl(amount(500), date("14/01/2021"));
+
+        account.printStatement(System.out);
+
+    }
+
+
+
+}
